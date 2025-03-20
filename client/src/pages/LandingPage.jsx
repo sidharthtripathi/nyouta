96a4808315108eb6f8d9e1b24ea1ebdc9ebdda69
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-03-20 17:58:18 +0530

    minor changes

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -5,78 +5,81 @@ import pigeonImg from "../assets/images/pigeons.png";
 export default function LandingPage() {
   return (
     <main className="space-y-8">
-      <section className="text-end flex w-full justify-between items-top h-72">
-        <img src={flower} className="h-full min-w-fit" alt="flowers img" />
+      <section className="text-end flex-wrap flex w-full justify-between items-top pl-2 pr-12">
+        <img src={flower} className="max-h-72 min-w-fit" alt="flowers img" />
         <div>
           <div className=" border-b-2">
             <div className=" h-14 w-32 inline-flex justify-end items-star rounded-t-full">
               <img src={pigeonImg} className="w-2/3 min-w-fit" alt="pigeons" />
             </div>
             <p className="text-8xl uppercase">Wedding</p>
-            <p className="text-2xl uppercase">Ceremony</p>
+            <p className="text-2xl uppercase mb-2">Ceremony</p>
           </div>
           <div className=" border-b-2">
-            <p className="text-5xl">Bride and Groom</p>
+            <p className="text-5xl my-2">Bride and Groom</p>
           </div>
-          <div className="text-lg">
-            <p >April 25th, 2025</p>
+          <div className="text-lg mt-2">
+            <p>April 25th, 2025</p>
             <time>7:00PM</time>
           </div>
         </div>
       </section>
-      <section className="flex justify-between items-center h-72 text-center">
-        <div className="flex flex-col justify-between">
+      <section className="flex justify-between items-center text-center h-72 pr-4 pl-14">
+        <div className="flex flex-col justify-between h-full">
           <div>
-            <p className="text-5xl">Save the Date</p>
-            <p>April 25th 2025</p>
-            <time>7:00PM</time>
+            <p className="text-5xl mb-2">Save the Date</p>
+            <p className="text-lg">April 25th 2025</p>
+            <time className="text-lg">7:00PM</time>
           </div>
-          <div>
+          <div className="text-lg">
             <p>Venue</p>
             <p>Venue name here</p>
           </div>
         </div>
         <div className="h-full flex items-center gap-4">
           <img src={coupleImg} className="h-1/2 min-w-fit" alt="couple img" />
-          <p>
+          <p className="text-5xl leading-20">
             Bride <br /> & <br /> Groom
           </p>
         </div>
         <img src={flower} className="h-full min-w-fit" alt="flower" />
       </section>
-      <section className="flex h-72 justify-between text-center">
-        <div className="grow">
-          <h2>About us</h2>
-          <p>
-            HI THERE! WE’RE [YOUR NAME] & [PARTNER’S NAME], TWO SOULS WHO FOUND
-            EACH OTHER AND HAVE BEEN BUILDING A LIFETIME OF LOVE EVER SINCE. OUR
-            JOURNEY BEGAN [INSERT HOW/WHERE YOU MET], AND WHAT STARTED AS [A
-            SPARK/A FRIENDSHIP/AN ADVENTURE] HAS GROWN INTO A DEEP AND ENDURING
-            CONNECTION. TOGETHER, WE’VE SHARED LAUGHTER, CHALLENGES, DREAMS, AND
-            COUNTLESS UNFORGETTABLE MOMENTS THAT HAVE BROUGHT US CLOSER EACH
-            DAY. WE’RE SO EXCITED TO CELEBRATE THIS NEXT CHAPTER OF OUR LOVE
-            STORY WITH THE PEOPLE WHO MEAN THE MOST TO US—OUR FAMILY AND
-            FRIENDS. YOUR LOVE, SUPPORT, AND BLESSINGS HAVE SHAPED OUR LIVES IN
-            MORE WAYS THAN WE CAN EXPRESS, AND HAVING YOU BY OUR SIDE AS WE SAY
-            “I DO” MAKES THIS DAY EVEN MORE SPECIAL. THANK YOU FOR BEING PART OF
-            OUR STORY. WE CAN’T WAIT TO SHARE THIS BEAUTIFUL MOMENT WITH YOU!
-            WITH LOVE, [YOUR NAME] & [PARTNER’S NAME]
-          </p>
+      <section className="flex justify-between items-start text-center">
+        <div className=" flex items-center">
+          <div className="px-4">
+            <h2 className="text-5xl mb-8">About us</h2>
+            <p className="text-sm">
+              HI THERE! WE’RE [YOUR NAME] & [PARTNER’S NAME], TWO SOULS WHO
+              FOUND EACH OTHER AND HAVE BEEN BUILDING A LIFETIME OF LOVE EVER
+              SINCE. OUR JOURNEY BEGAN [INSERT HOW/WHERE YOU MET], AND WHAT
+              STARTED AS [A SPARK/A FRIENDSHIP/AN ADVENTURE] HAS GROWN INTO A
+              DEEP AND ENDURING CONNECTION. TOGETHER, WE’VE SHARED LAUGHTER,
+              CHALLENGES, DREAMS, AND COUNTLESS UNFORGETTABLE MOMENTS THAT HAVE
+              BROUGHT US CLOSER EACH DAY. WE’RE SO EXCITED TO CELEBRATE THIS
+              NEXT CHAPTER OF OUR LOVE STORY WITH THE PEOPLE WHO MEAN THE MOST
+              TO US—OUR FAMILY AND FRIENDS. YOUR LOVE, SUPPORT, AND BLESSINGS
+              HAVE SHAPED OUR LIVES IN MORE WAYS THAN WE CAN EXPRESS, AND HAVING
+              YOU BY OUR SIDE AS WE SAY “I DO” MAKES THIS DAY EVEN MORE SPECIAL.
+              THANK YOU FOR BEING PART OF OUR STORY. WE CAN’T WAIT TO SHARE THIS
+              BEAUTIFUL MOMENT WITH YOU! WITH LOVE, [YOUR NAME] & [PARTNER’S
+              NAME]
+            </p>
+          </div>
         </div>
         <div className="flex h-full min-w-fit">
           <img
             src={valley}
-            className="h-full w-60 object-cover rounded-t-full"
+            className="h-72 w-60 object-cover rounded-t-full"
             alt="valley"
           />
-          <img src={flower} className="h-full min-w-fit " alt="flower" />
+          <img src={flower} className="h-72 " alt="flower" />
         </div>
       </section>
-      <section className="h-72 flex items-start text-center">
-        <img src={flower} className="h-full min-w-fit" alt="flower" />
-        <div className="grow">
-          <h2>Our Story</h2>
-          <p>
+      <section className="min-h-fit flex items-start text-center">
+        <img src={flower} className="h-72 min-w-fit" alt="flower" />
+        <div className="grow text-center">
+          <h2 className="text-5xl mb-4">Our Story</h2>
+          <p className="text-lg px-36">
             IT ALL BEGAN [INSERT WHERE AND HOW YOU MET]. WHAT SEEMED LIKE [A
             CASUAL ENCOUNTER/DESTINY/A PERFECT COINCIDENCE] QUICKLY TURNED INTO
             SOMETHING UNFORGETTABLE. FROM THE VERY BEGINNING, WE KNEW THERE WAS
@@ -87,58 +90,54 @@ export default function LandingPage() {
       <section className="flex h-72 text-center">
         <img src={flower} className="h-full min-w-fit" alt="flower" />
         <div className="grow">
-          <h2>Wedding Programs</h2>
+          <h2 className="text-5xl mb-4">Wedding Programs</h2>
           {/* <WeddingProgram /> */}
-          {
-            weddingPrograms.map(({time,events,venueName})=>(
-              <WeddingProgram time={time} events={events} venueName={venueName} />
-            ))
-
-          }
+          {weddingPrograms.map(({ time, events, venueName }) => (
+            <WeddingProgram time={time} events={events} venueName={venueName} />
+          ))}
         </div>
       </section>
       <section className="h-72 flex justify-between items-start">
-        <div>
-          <h2>Event Name</h2>
-          <p>Monday 21st april</p>
+        <div className="text-center">
+          <h2 className="text-5xl">Event Name</h2>
+          <p className="text-xl">Monday 21st april</p>
           <ul>
-            <li>event 1</li>
-            <li>event 1</li>
-            <li>event 1</li>
+            <li>EVENT-1 NAME - TIME</li>
+            <li>EVENT-1 NAME - TIME</li>
+            <li>EVENT-1 NAME- TIME</li>
           </ul>
           <div>icon</div>
           <div>
-            <p>Venue</p>
-            <p>Venue name here</p>
+            <p className="text-sm">Venue</p>
+            <p className="text-xl">Venue name here</p>
           </div>
         </div>
         <img src={flower} className="h-full min-w-fit" alt="flower" />
-        <div>
-          <h2>Event Name</h2>
-          <p>Monday 21st april</p>
+        <div className="text-center">
+          <h2 className="text-5xl">Event Name</h2>
+          <p className="text-xl">Monday 21st april</p>
           <ul>
-            <li>event 1</li>
-            <li>event 1</li>
-            <li>event 1</li>
+            <li>EVENT-1 NAME - TIME</li>
+            <li>EVENT-1 NAME - TIME</li>
+            <li>EVENT-1 NAME- TIME</li>
           </ul>
           <div>icon</div>
           <div>
-            <p>Venue</p>
-            <p>Venue name here</p>
+            <p className="text-sm">Venue</p>
+            <p className="text-xl">Venue name here</p>
           </div>
         </div>
       </section>
       <section className="flex h-72 items-center justify-between">
         <img src={flower} className="h-full min-w-fit" alt="flower" />
-        <p>
+        <p className="text-5xl">
           Wedding <br /> Timeline
         </p>
-        <div>{
-
-          timelines.map(({time,events})=>(
+        <div>
+          {timelines.map(({ time, events }) => (
             <Timeline time={time} events={events} />
-          ))
-          }</div>
+          ))}
+        </div>
       </section>
     </main>
   );
@@ -147,14 +146,28 @@ export default function LandingPage() {
 // events => {name,time}[]
 
 const weddingPrograms = [
-  { time: "sdfs", events: [{ name: "asdf", time: "asdf" },{ name: "asdf", time: "asdf" }], venueName: "asdf" },
-  { time: "sdfs", events: [{ name: "asdf", time: "asdf" },{ name: "asdf", time: "asdf" }], venueName: "asdf" }
+  {
+    time: "MONDAY, 2XST APRIL, 2025",
+    events: [
+      { name: "EVENT-1 NAME", time: "TIME" },
+      { name: "EVENT-2 NAME", time: "TIME" },
+    ],
+    venueName: "VENUE : VENUE NAME HERE",
+  },
+  {
+    time: "MONDAY, 2XND APRIL, 2025",
+    events: [
+      { name: "EVENT-1 NAME", time: "TIME" },
+      { name: "EVENT-2 NAME", time: "TIME" },
+    ],
+    venueName: "VENUE : VENUE NAME HERE",
+  },
 ];
 
 function WeddingProgram({ time, events, venueName }) {
   return (
-    <div>
-      <title>{time}</title>
+    <div className="text-lg mt-2">
+      <p className="text-2xl">{time}</p>
       {events.map(({ name, time }) => (
         <p>
           {name},{time}
@@ -166,17 +179,38 @@ function WeddingProgram({ time, events, venueName }) {
 }
 
 const timelines = [
-  {time:"sdfa",events : [{time:"asdf",eventName : "asdf"},{time:"asdf",eventName : "asdf"},{time:"asdf",eventName : "asdf"}]},
-  {time:"sdfa",events : [{time:"asdf",eventName : "asdf"},{time:"asdf",eventName : "asdf"},{time:"asdf",eventName : "asdf"}]},
-  {time:"sdfa",events : [{time:"asdf",eventName : "asdf"},{time:"asdf",eventName : "asdf"},{time:"asdf",eventName : "asdf"}]}
-]
+  {
+    time: "MONDAY, 2XST APRIL, 2025",
+    events: [
+      { time: "EVENT NAME 1", eventName: "00.00 AM/P" },
+      { time: "EVENT NAME 2", eventName: "00.00 AM/P," },
+      { time: "EVENT NAME 3", eventName: "00.00 AM/P," },
+    ],
+  },
+  {
+    time: "TUESDAY, 2XST APRIL, 2025",
+    events: [
+      { time: "EVENT NAME 1", eventName: "00.00 AM/P" },
+      { time: "EVENT NAME 2", eventName: "00.00 AM/P," },
+      { time: "EVENT NAME 3", eventName: "00.00 AM/P," },
+    ],
+  },
+  {
+    time: "WEDNESDAY, 2XST APRIL, 2025",
+    events: [
+      { time: "EVENT NAME 1", eventName: "00.00 AM/P" },
+      { time: "EVENT NAME 2", eventName: "00.00 AM/P," },
+      { time: "EVENT NAME 3", eventName: "00.00 AM/P," },
+    ],
+  },
+];
 // events => {name,time}[]
 function Timeline({ time, events }) {
   return (
     <div>
-      <div>{time}</div>
+      <div className="text-xl mt-2">{time}</div>
       {events.map(({ eventName, time }) => (
-        <div>
+        <div className="text-sm">
           {eventName},{time}
         </div>
       ))}
